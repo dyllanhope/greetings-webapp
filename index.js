d0eea8fb388bc7c6b1d07392732cec925272be0b
Dyllan Hope <dyllanjhope@gmail.com>
2019-06-07 16:27:36 +0200

    language choice support added

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 "use strict";
-
 const express = require('express');
-const moment = require('moment');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const flash = require('express-flash');
+const session = require('express-session');
 
 const nameTrack = require("./nameTrack");
 
 const app = express();
 const nameTracker = nameTrack();
+
+app.use(session({
+    secret: "secret message",
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(flash());
 
 const handlebarSetup = exphbs({
     partialsDir: "./views",
@@ -25,15 +33,43 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 
-app.get("/",function(req,res){
-    res.render("index",{
+app.get("/", function (req, res) {
+    res.render("index", {
         name: nameTracker.name(),
-        counter: nameTracker.counter()
+        counter: nameTracker.counter(),
+        greet: nameTracker.greeting()
     });
 });
 
-app.post('/greet',function(req,res){
-    nameTracker.greet(req.body.inputName);
+app.post('/greet', function (req, res) {
+    if (req.body.languageChoice) {
+        if ((req.body.inputName).trim()) {
+            nameTracker.lang(req.body.languageChoice);
+            nameTracker.greet(req.body.inputName);
+            res.redirect('/');
+        } else {
+            req.flash("info", "Enter a name!");
+            res.redirect('/');
+        }
+    }else {
+        req.flash("info", "Select a language!");
+        res.redirect('/');
+    }
+});
+
+app.get("/greeted", function (req, res) {
+    res.render('names', {
+        nameList: nameTracker.nameList()
+    });
+});
+
+app.get("/counter/:nameChoice", function (req, res) {
+    let name = req.params.nameChoice;
+    req.flash("amount", "Hello, " + name + " has been greeted " + nameTracker.amntFor(name) + " times");
+    res.redirect("/greeted");
+});
+
+app.post("/return", function (req, res) {
     res.redirect('/');
 });
 
--- nameTrack.js
+++ nameTrack.js
@@ -1,55 +1,72 @@
 module.exports = function (nameList) {
     var namesGreeted = nameList || {};
-    var errName = '';
+    var nameData = []
     var newName = '';
+    var langGreet = '';
 
     function loadItems() {
         namesGreeted = {};
     }
+
     function addName(userName) {
-        newName = userName;
-        var upFirst = userName.toUpperCase();
+        newName = userName.charAt(0).toUpperCase() + (userName.slice(1)).toLowerCase();
         const regex = /\d/;
-        var numTest = regex.test(userName);
-        errName = userName;
-        if (numTest === false) {
-            if (namesGreeted[upFirst] === undefined) {
-                namesGreeted[upFirst] = 0;
+        var numTest = regex.test(newName);
+        if (userName.trim()) {
+            if (numTest === false) {
+                if (namesGreeted[newName] === undefined) {
+                    namesGreeted[newName] = 0;
+                    nameData.push({ name: newName, times: 1 });
+                } else {
+                    for (var x = 0; x < nameData.length; x++) {
+                        if (newName === nameData[x].name) {
+                            nameData[x].times++;
+                        }
+                    }
+                }
             }
         }
-        // if (lang === "english") {
-        //     return "Hello, " + userName;
-        // } else if (lang === "afrikaans") {
-        //     return "Hallo, " + userName;
-        // } else if (lang === "isixhosa") {
-        //     return "Molo, " + userName;
-        // } else {
-        //     return "Hello, " + userName;
-        // }
-
+    }
+    function determineGreeting() {
+        return langGreet;
     }
     function displayCounter() {
-        var numberOfNames = Object.keys(namesGreeted);
-        return numberOfNames.length;
+        return nameData.length;
     }
-    function displayString() {
-        return namesGreeted;
-    }
-    function errorCheck() {
-        if (errName === '') {
-            return 'error';
-        }
-    }
-    function displayName(){
+    function displayName() {
         return newName;
     }
+    function displayNames() {
+        return nameData;
+    }
+    function displayGreetedFor(name) {
+        var num = 0;
+        for (var x = 0; x < nameData.length; x++) {
+            if (name === nameData[x].name) {
+                num = nameData[x].times;
+            }
+        }
+        return num;
+    }
+    function loadLanguage(lang){
+        if (lang === "english") {
+            langGreet = "Hello, ";
+        } else if (lang === "afrikaans") {
+            langGreet = "Hallo, ";
+        } else if (lang === "isixhosa") {
+            langGreet = "Molo, ";
+        } 
+    }
+
 
     return {
         greet: addName,
         counter: displayCounter,
         load: loadItems,
-        items: displayString,
-        error: errorCheck,
-        name: displayName
+        name: displayName,
+        nameList: displayNames,
+        amntFor: displayGreetedFor,
+        greeting : determineGreeting,
+        lang : loadLanguage
     }
 }
